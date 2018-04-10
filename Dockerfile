46607f012be96f5e4fbb386084c0ed89522e7339
Johan Mulder <johan@localhost.nl>
2018-04-10 22:56:35 +0200

    Replaced Java with OpenJDK.
    Java on dockerhub turned out to be deprecated in favor of OpenJDK.
    Now using the alpine linux version of OpenJDK 8, which is a lot smaller
    than the Debian based one.
    
    Also fixed the formatting in the DemoController a bit.

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ WORKDIR .
 COPY . .
 RUN mvn package
 
-FROM java:8-jre as run
+FROM openjdk:8-jre-alpine as run
 RUN mkdir /app
 WORKDIR /app
 COPY --from=build target/dockerdemo-0.0.1-SNAPSHOT.jar .
 EXPOSE 8080
+USER nobody
 ENTRYPOINT java -jar dockerdemo-0.0.1-SNAPSHOT.jar
--- src/main/java/nl/localhost/dockerdemo/DemoController.java
+++ src/main/java/nl/localhost/dockerdemo/DemoController.java
@@ -1,6 +1,8 @@
 package nl.localhost.dockerdemo;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,7 @@ public class DemoController
 	@GetMapping("/")
 	public ResponseEntity<String> demo()
 	{
-		StringBuilder sb = new StringBuilder("Hi there from a controller in a Spring Boot application\n");
+		StringBuilder sb = new StringBuilder("Hi there from a controller in a Spring Boot application\n\n");
 		sb.append("Running on Java version: ");
 		sb.append(System.getProperty("java.version"));
 		sb.append("\n");
@@ -22,6 +24,9 @@ public class DemoController
 		sb.append(System.getenv("HOSTNAME"));
 		sb.append("\n");
 
-		return new ResponseEntity<>(sb.toString(), HttpStatus.OK);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", MediaType.TEXT_PLAIN_VALUE);
+
+		return new ResponseEntity<>(sb.toString(), headers, HttpStatus.OK);
 	}
 }
